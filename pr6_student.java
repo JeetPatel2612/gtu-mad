5edee7af47b550f1007f2877937dbc09f6c2db38
JeetPatel2612 <95037295+JeetPatel2612@users.noreply.github.com>
2024-08-27 10:54:11 +0530

    Update pr6_student.java

--- pr6_student.java
+++ pr6_student.java
@@ -1,1 +1,37 @@
-// Student java
+package com.ssasit.pr6;
+
+public class student {
+    String name,address;
+    int image;
+
+    public student(String name, String address, int image) {
+        this.name = name;
+        this.address = address;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+}
